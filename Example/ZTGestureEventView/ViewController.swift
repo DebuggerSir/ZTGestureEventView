db82418f2cebf0c2bf864b6d9c61f791fd177270
zhangtian <zhangtian@sdiread.com>
2020-08-26 19:40:37 +0800

    Initial commit

---
+++ Example/ZTGestureEventView/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ZTGestureEventView
+//
+//  Created by SkyerWalker on 08/26/2020.
+//  Copyright (c) 2020 SkyerWalker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
